70b55fc2c4311394548595403d0ae517d5691aac
adamfitzhugh <fitzadd@gmail.com>
2021-01-21 19:59:55 +0000

    Added new content

---
+++ acloudguru/vpc.tf
@@ -0,0 +1,9 @@
+# VPC
+
+## VPC Router
+AWS Reserved IP addresses (example subnet 10.0.64.0/21):
+10.0.64.0 - Network address
+10.0.64.1 - VPC Router address (default gateway)
+10.0.64.2 - IP address of DNS server
+10.0.64.3 - Reserved for future use
+10.0.71.255 - Network broadcast address
