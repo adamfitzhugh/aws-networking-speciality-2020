93516621557e8e2fa9ade943210df8410efd635d
adamfitzhugh <fitzadd@gmail.com>
2021-01-27 10:53:15 +0000

    Updated VPC

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,8 +1,3 @@
-variable "region" {
-  description = "Default VPC region"
-  type        = string
-  default     = "eu-west-2"
-}
 variable "primary_cidr" {
   description = "The CIDR block for the VPC"
   type        = string
--- networking/main.tf
+++ networking/main.tf
@@ -2,6 +2,7 @@ module "vpc" {
   source = "../modules/vpc"
 
   primary_cidr       = var.primary_cidr
+  azs = var.azs
   database_subnets   = var.database_subnets
   web_server_subnets = var.web_server_subnets
 }
