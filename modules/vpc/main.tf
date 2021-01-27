d84bddbaba2299925ef42b694b1e0ed050c3c090
adamfitzhugh <fitzadd@gmail.com>
2021-01-27 11:32:39 +0000

    Updated DB subnet~

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -28,6 +28,6 @@ resource "aws_subnet" "web_server_subnet" {
   cidr_block        = var.web_server_subnets[count.index]
 
   tags = {
-    Name = "Web Server Subnet - var.azs[count.index]"
+    Name = "Web Server Subnet -" ${var.azs[count.index]}
   }
 }
