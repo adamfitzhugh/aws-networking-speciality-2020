64409d273cb2bedefac8d0ebf1a59e5538a5caff
adamfitzhugh <fitzadd@gmail.com>
2021-01-28 09:01:35 +0000

    Added SG

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -33,7 +33,7 @@ resource "aws_subnet" "web_server_subnet" {
 }
 
 resource "aws_security_group" "test_sg" {
-  name        = "${var.region}-sg"
+  name        = "${var.region}-${var.region}"
   description = "Allow TLS inbound traffic"
   vpc_id      = aws_vpc.main_vpc.id
 }
