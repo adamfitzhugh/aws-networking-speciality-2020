bc950d885dc9f32591c9180007c7b692e6161614
adamfitzhugh <fitzadd@gmail.com>
2021-02-01 10:44:47 +0000

    Amended VPC

--- networking/vpc.tf
+++ networking/vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source = "git::https://github.com/adamfitzhugh/aws-terraform-vpc-module.git"
 
   name = "Primary VPC"
-  primary_cidr       = var.primary_cidr
+  cidr_block       = var.cidr_block
   azs = var.azs
   private_subnets = var.private_subnets
 }
