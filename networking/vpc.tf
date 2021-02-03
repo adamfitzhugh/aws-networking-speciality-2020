c174c7aa74a589d2cf24736edf683f97838a7af0
adamfitzhugh <fitzadd@gmail.com>
2021-02-03 14:33:23 +0000

    Added VPC name

--- networking/vpc.tf
+++ networking/vpc.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "git::https://github.com/adamfitzhugh/aws-terraform-vpc-module.git"
 
-  name = var.name
+  name = "my-vpc"
   cidr_block       = var.cidr_block
   azs = var.azs
   private_subnets = var.private_subnets
